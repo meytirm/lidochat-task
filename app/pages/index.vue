77bb33ad90dbcc2d30a79dcce4b3a8770a6eb40a
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-19 15:19:35 +0330

    feat(pages): add data fetching on mounted in index.vue
    Introduced an API call using `$api` in the `onMounted` lifecycle to fetch data in `index.vue`. Integrated runtime configuration and logging of API responses for debugging purposes.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script setup lang="ts">
+const { $api } = useNuxtApp()
+const config = useRuntimeConfig()
+const fetchData = async () => {
+  const res = await $api.post(`accounts:signInWithCustomToken?key=`)
+  console.log(res)
+}
 
+onMounted(() => {
+  fetchData()
+})
 </script>
 
 <style scoped>
