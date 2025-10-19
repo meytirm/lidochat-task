3f57dec3ab8092eadfb73bb4fe7f370b2351cadb
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-19 21:43:29 +0330

    feat(service): update AuthService methods to accept payload
    Updated `signUp` and `signIn` methods in `AuthService` to accept payload objects for better flexibility and reusability. Removed hardcoded user credentials.

--- app/service/auth/index.ts
+++ app/service/auth/index.ts
@@ -4,15 +4,18 @@ export class AuthService {
   constructor(private readonly api: ServiceApi) {
   }
 
-  signUp() {
-
+  signUp(payload: {
+    email: string
+    password: string
+  }) {
+    return this.api.post('/api/firebase-signup', payload)
   }
 
-  signIn() {
-    return this.api.post('/api/firebase-login', {
-      email: 'test@test.com',
-      password: 'testtesttest',
-      returnSecureToken: true,
-    })
+  signIn(payload: {
+    email: string
+    password: string
+    returnSecureToken: boolean
+  }) {
+    return this.api.post('/api/firebase-login', payload)
   }
 }
