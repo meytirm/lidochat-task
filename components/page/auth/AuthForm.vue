4274dbe63f6eed2cd088bd9c3c92d8c6c30ddbb2
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-20 11:22:01 +0330

    feat(auth): add AuthForm component
    Introduced `AuthForm.vue` component to handle authentication forms. Added `authType` prop to differentiate between `sign-in` and `sign-up` modes. Includes a basic template and placeholder for scoped styles.

---
+++ components/page/auth/AuthForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    auth form
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  authType: 'sign-in' | 'sign-up'
+}>()
+</script>
+
+<style scoped>
+
+</style>
