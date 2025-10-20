a6fac4c302c3877f3f3ad335cbec41373ec449be
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-20 09:38:35 +0330

    feat(types): add FirebaseUserResponse interface
    Defined `FirebaseUserResponse` interface in `types/firebase.ts` to represent detailed user properties returned by Firebase. Included nested `ProviderUserInfo` type for provider-linked account details.

--- types/firebase.ts
+++ types/firebase.ts
@@ -9,3 +9,24 @@ export interface FirebaseAuthResponse {
 
 export type FirebaseSignInResponse = FirebaseAuthResponse
 export type FirebaseSignupResponse = Omit<FirebaseAuthResponse, 'registered'>
+
+interface ProviderUserInfo {
+  providerId: string
+  federatedId: string
+}
+
+export interface FirebaseUserResponse {
+  localId: string
+  email: string
+  emailVerified: boolean
+  displayName: string
+  providerUserInfo: ProviderUserInfo[]
+  photoUrl: string
+  passwordHash: string
+  passwordUpdatedAt: number // in milliseconds
+  validSince: string // in seconds
+  disabled: boolean
+  lastLoginAt: string // in milliseconds
+  createdAt: string // in milliseconds
+  customAuth: boolean
+}
