2bf1ee6fa06c4feac64d3a7def34d9fec235fe9c
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-20 09:38:47 +0330

    feat(types): add FirebaseUserResponse interface
    Defined `FirebaseUserResponse` interface in `types/firebase.ts` to represent detailed user properties returned by Firebase. Included nested `ProviderUserInfo` type for provider-linked account details.

--- app/service/user/index.ts
+++ app/service/user/index.ts
@@ -1,10 +1,12 @@
 import type { ServiceApi } from '~/service/service-api'
+import type { AxiosResponse } from 'axios'
+import type { FirebaseUserResponse } from '~~/types/firebase'
 
-export class AuthService {
+export class UserService {
   constructor(private readonly api: ServiceApi) {
   }
 
-  getData() {
-    return this.api.post('/api/firebase-user-data', {})
+  getData(idToken: string): Promise<AxiosResponse<FirebaseUserResponse[]>> {
+    return this.api.post('/api/firebase-user-data', { idToken })
   }
 }
