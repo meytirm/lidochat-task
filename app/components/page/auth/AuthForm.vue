8dfdcfb21b17723b9ad81822f4baa416781173ab
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-20 12:11:36 +0330

    feat(auth): introduce reusable AuthForm component
    Added `AuthForm.vue` to create a unified authentication form for both `sign-in` and `sign-up` workflows. Integrated `authType` prop to toggle form fields dynamically based on the authentication mode. Included scoped styles placeholder for future enhancements.

---
+++ app/components/page/auth/AuthForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <BCard>
+    <div class="d-flex justify-content-center">
+      {{ props.authType === 'sign-in' ? 'Sign In' : 'Sign Up' }}
+    </div>
+    <BForm>
+      <BFormFloatingLabel
+        label="Email address"
+        label-for="floatingEmail"
+        class="my-2"
+      >
+        <BFormInput
+          id="floatingEmail"
+          v-model="authForm.email"
+          type="email"
+          placeholder="Email address"
+        />
+      </BFormFloatingLabel>
+      <BFormFloatingLabel
+        label="Password"
+        label-for="floatingPassword"
+        class="my-2"
+      >
+        <BFormInput
+          id="floatingPassword"
+          v-model="authForm.password"
+          type="password"
+          placeholder="Password"
+        />
+      </BFormFloatingLabel>
+      <BFormFloatingLabel
+        v-if="props.authType === 'sign-up'"
+        label="Confirm Password"
+        label-for="floatingConfirmPassword"
+        class="my-2"
+      >
+        <BFormInput
+          id="floatingConfirmPassword"
+          v-model="authForm.confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+        />
+      </BFormFloatingLabel>
+      <BButton
+        type="submit"
+        variant="primary"
+      >
+        Submit
+      </BButton>
+    </BForm>
+  </BCard>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  authType: 'sign-in' | 'sign-up'
+}>()
+
+const authForm = reactive({
+  email: '',
+  password: '',
+  confirmPassword: '',
+})
+</script>
+
+<style scoped>
+
+</style>
