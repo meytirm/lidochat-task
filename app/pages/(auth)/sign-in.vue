3077f3f30bc930d2cf464f60d5ac17dfbaf8d8e3
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-20 12:13:58 +0330

    feat(auth): integrate AuthForm into sign-in page
    Replaced placeholder text in `sign-in.vue` with the reusable `AuthForm` component to standardize the sign-in form. Added necessary import for `AuthForm`.

--- app/pages/(auth)/sign-in.vue
+++ app/pages/(auth)/sign-in.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    sign in
+    <AuthForm auth-type="sign-in" />
   </div>
 </template>
 
 <script setup lang="ts">
+import AuthForm from '~/components/page/auth/AuthForm.vue'
+
 definePageMeta({
   layout: 'auth',
 })
