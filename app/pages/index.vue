6f95037aaab19073c904ee948faf0f79a1b14519
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-20 12:12:04 +0330

    refactor(index): remove unused authentication logic
    Deleted obsolete authentication service imports and related code in the `index.vue` script. Cleaned up the template to improve maintainability.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,26 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { AuthService } from '~/service/auth'
 
-const { $api } = useNuxtApp()
-const authService = new AuthService($api)
-const fetchData = async () => {
-  const res = await authService.signIn()
-  // const res = await $fetch('/api/firebase-login', {
-  //   method: 'POST',
-  //   body: {
-  //     email: 'test@test.com',
-  //     password: 'testtesttest',
-  //     returnSecureToken: true,
-  //   },
-  // })
-  console.log(res)
-}
-
-onMounted(() => {
-  fetchData()
-})
 </script>
 
 <style scoped>
