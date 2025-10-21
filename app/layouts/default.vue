32d56306943a469cdc8bed4cee5495d164c4f4ab
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-21 10:33:10 +0330

    feat(layouts): add navbar with user info and sign-out functionality
    Integrated a responsive navbar in `default.vue` with user information display and sign-out functionality. Applied local storage persistence for user data and enhanced user session handling, including cookie-based access token usage.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,74 @@
 <template>
   <div>
-    <h1>Default Layout</h1>
+    <BNavbar
+      v-b-color-mode="'dark'"
+      toggleable="lg"
+      variant="primary"
+    >
+      <BNavbarBrand href="#">
+        Dashboard
+      </BNavbarBrand>
+      <BNavbarToggle target="nav-collapse" />
+      <BCollapse
+        id="nav-collapse"
+        is-nav
+      >
+        <!-- Right aligned nav items -->
+        <BNavbarNav class="ms-auto mb-2 mb-lg-0">
+          <BNavItemDropdown
+            v-if="userData"
+            right
+          >
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>{{ userData.email }}</em>
+            </template>
+            <BDropdownItem
+              href="#"
+              @click="signOut"
+            >
+              Sign Out
+            </BDropdownItem>
+          </BNavItemDropdown>
+        </BNavbarNav>
+      </BCollapse>
+    </BNavbar>
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
+import { useStore } from 'vuex'
+import type { FirebaseUserResponse } from '~~/types/firebase'
 
+const store = useStore()
+const userData = computed<FirebaseUserResponse>(() => store.state.user)
+
+async function signOut() {
+  try {
+    await store.dispatch('signOut')
+    localStorage.removeItem('userInformation')
+    await navigateTo('/sign-in')
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+onMounted(async () => {
+  const userInformationStorage = localStorage.getItem('userInformation')
+  console.log(userInformationStorage)
+  if (userInformationStorage) {
+    const userInformation = JSON.parse(userInformationStorage)
+    store.commit('setUser', userInformation)
+  }
+  else {
+    const accessToken = useCookie('accessToken').value
+    const userData = await store.dispatch('getUserData', accessToken)
+    if (userData) {
+      localStorage.setItem('userInformation', JSON.stringify(userData))
+    }
+  }
+})
 </script>
 
 <style scoped>
