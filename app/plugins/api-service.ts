262ec1835362cfb2e06ba10b245bb5e75f4efe28
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-21 19:16:02 +0330

    feat(api): add toast notifications for API service actions
    Introduced `createToast` function to enable toast notifications in API service. Updated `ServiceApi` initialization to include `createToast` for enhanced user feedback during API actions.

--- app/plugins/api-service.ts
+++ app/plugins/api-service.ts
@@ -1,12 +1,24 @@
 import { defineNuxtPlugin, useRuntimeConfig, useCookie } from '#app'
 import { ServiceApi } from '~/service/service-api'
+import type { ToastOptions } from '~~/types/toast'
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
+  const { create } = useToast()
   const baseUrl = config.public.clientApiBase
   const accessToken = () => useCookie('accessToken').value
+  const createToast = ({ title, body, variant }: ToastOptions) => {
+    create({
+      title,
+      body,
+      variant,
+      progressProps: {
+        variant: 'secondary',
+      },
+    })
+  }
 
-  const apiService = new ServiceApi(baseUrl, accessToken)
+  const apiService = new ServiceApi(baseUrl, accessToken, createToast)
   return {
     provide: {
       api: apiService,
